6268a86c29c01cb62858e0cc9fdfc8635d954e5e
Ruban <65346010+sk-ruban@users.noreply.github.com>
2024-09-11 15:33:26 +0800

    Add 2220 - Minimum Bit Flips to Convert Number

---
+++ 2220 - Minimum Bit Flips to Convert Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        res = 0
+
+        while start or goal:
+            if start % 2 != goal % 2:
+                res += 1
+            start = start >> 1
+            goal = goal >> 1
+
+        return res
