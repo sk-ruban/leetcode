08dc9ae5313704ab6575026ee43be1c76b1047c9
Ruban <65346010+sk-ruban@users.noreply.github.com>
2025-09-18 01:20:56 +0800

    Update 0844 - Backspace String Compare

--- 0844 - Backspace String Compare.py
+++ 0844 - Backspace String Compare.py
@@ -1,14 +1,27 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        def checker(string: str) -> list:
-            res = []
-            for char in string:
-                if char == '#' and res:
-                    res.pop(-1)
-                elif char == "#":
-                    continue
+        def backspace(c, i):
+            count = 0
+            while i >= 0:
+                if c[i] == '#':
+                    count += 1
+                elif count > 0:
+                    count -= 1
                 else:
-                    res.append(char)
-            return res
+                    return i, c[i]
+                i -= 1
+            return -1, None
 
-        return checker(s) == checker(t)
+        i, j = len(s) - 1, len(t) - 1
+
+        while i >= 0 or j >= 0:
+            i, c1 = backspace(s, i)
+            j, c2 = backspace(t, j)
+
+            if c1 != c2:
+                return False
+
+            if i >= 0: i -= 1
+            if j >= 0: j -= 1
+
+        return True
