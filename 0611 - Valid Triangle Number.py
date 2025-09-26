fb970c4e7dcfeb1356e90b3f3e3ad65d7893dfa9
Ruban <65346010+sk-ruban@users.noreply.github.com>
2025-09-26 16:04:59 +0800

    Add 0611 - Valid Triangle Number

---
+++ 0611 - Valid Triangle Number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        count = 0
+
+        for k in range(2, n):
+            i, j = 0, k-1
+
+            while i < j:
+                if nums[i] + nums[j] > nums[k]:
+                    count += (j-i)
+                    j -= 1
+                else:
+                    i += 1
+
+        return count
