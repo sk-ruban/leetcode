ce6a8ed90dbccc02209ec661607f53df21d147c6
Ruban <65346010+sk-ruban@users.noreply.github.com>
2025-11-30 02:36:05 +0800

    Add 2169 - Count Operations to Obtain Zero

---
+++ 2169 - Count Operations to Obtain Zero.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        ops = 0
+
+        while num1 != 0 and num2 != 0:
+            if num1 >= num2:
+                ops += num1 // num2
+                num1 %= num2
+            else:
+                ops += num2 // num1
+                num2 %= num1
+
+        return ops
