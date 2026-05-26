f481387566c50e8a017a83cf7a1d501e60e4f544
Ruban <65346010+sk-ruban@users.noreply.github.com>
2026-05-26 14:15:14 +0800

    Add 0703 - Kth Largest Element in a Stream

---
+++ 0703 - Kth Largest Element in a Stream.py
@@ -0,0 +1,16 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = []
+        for n in nums:
+            self.add(n)
+
+    def add(self, val: int) -> int:
+        if len(self.heap) < self.k:
+            heapq.heappush(self.heap, val)
+        else:
+            if val > self.heap[0]:
+                heapq.heappushpop(self.heap, val)
+
+        return self.heap[0]
