a5c19e9035120d10fb8c4bcc436a1047284ed92f
Ruban <65346010+sk-ruban@users.noreply.github.com>
2026-06-16 19:14:30 +0800

    Add 0019 - Remove Nth Node From End of List

---
+++ 0019 - Remove Nth Node From End of List.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+        slow, fast = dummy, dummy
+
+        for i in range(n):
+            fast = fast.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next
+
+        slow.next = slow.next.next
+
+        return dummy.next
