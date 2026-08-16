2e21d4faa8af173bcb4a304be7cc27a37447276a
Ruban <65346010+sk-ruban@users.noreply.github.com>
2026-08-17 01:20:46 +0800

    Add 1563 - Stone Game V

---
+++ 1563 - Stone Game V.py
@@ -0,0 +1,18 @@
+class Solution:
+    def stoneGameV(self, stoneValue: List[int]) -> int:
+        accum = [0] + list(accumulate(stoneValue))
+
+        @cache
+        def dp(l, r):
+            if l == r: return 0
+            best = 0
+            for i in range(l, r):
+                L = accum[i+1] - accum[l]
+                R = accum[r+1] - accum[i+1]
+                if L < R: best = max(best, L + dp(l, i))
+                elif L > R: best = max(best, R + dp(i+1, r))
+                else: best = max(best, L + dp(l, i), R + dp(i+1, r))
+
+            return best
+
+        return dp(0, len(stoneValue)-1)
